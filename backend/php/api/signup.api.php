d9a5db9dd635a9ee6f157a80e7490cf77f05b3de
Alberto Tontoni <tontonialberto@outlook.it>
2022-07-24 21:58:45 +0200

    Improve if-else structure

--- backend/php/api/signup.api.php
+++ backend/php/api/signup.api.php
@@ -10,74 +10,85 @@
         "error" => "Empty request"
     );
 
-    if(isset(
-            $_POST["token"],
+    $error_happened = false;
+
+    if(!isset(
+        $_POST["token"],
+        $_POST["username"],
+        $_POST["password"],
+        $_POST["email"],
+        $_POST["userlevel"],
+        $_POST["classe"],
+        $_POST["nome"],
+        $_POST["cognome"],
+        $_POST["citta"])) {
+            $error_happened = true;
+    }
+
+    if(!$error_happened) {
+        $auth_result = token_verify($_POST["token"]);
+
+        if(!$auth_result["success"] || !$auth_result["isAdmin"]) {
+            $api_result["error"] = "Error: you cannot create new users";
+            $error_happened = true;
+        }
+    }
+
+    $mysqli = null;
+    if(!$error_happened) {
+        $mysqli = new mysqli($db_server, $db_username, $db_password, $db_name);
+
+        if($mysqli->connect_error) {
+            $api_result["error"] = "DB connect error: ".$mysqli->connect_error;
+            $error_happened = true;
+        }
+    }
+
+    if(!$error_happened) {
+        $query_insert_user = "INSERT INTO tab_utenti VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt_insert_user = $mysqli->prepare($query_insert_user);
+
+        if(!$stmt_insert_user) {
+            $api_result["error"] = "Statement prepare error: ".$mysqli->error;
+            $error_happened = true;
+        }
+    }
+            
+    if(!$error_happened) {
+        $ID_utente = NULL;
+        
+        if($_POST["classe"] == "NO") $_POST["classe"] = NULL;
+        if($_POST["citta"] == "NO") $_POST["citta"] = NULL;
+
+        $stmt_insert_user->bind_param("isssissss", 
+            $ID_utente,
             $_POST["username"],
-            $_POST["password"],
+            password_hash($_POST["password"], PASSWORD_DEFAULT),
             $_POST["email"],
             $_POST["userlevel"],
             $_POST["classe"],
             $_POST["nome"],
             $_POST["cognome"],
-            $_POST["citta"])) 
-    {
-        $auth_result = token_verify($_POST["token"]);
+            $_POST["citta"]);
 
-        if(!$auth_result["success"] || !$auth_result["isAdmin"]) 
-        {
-            $api_result["error"] = "Error: you cannot create new users";
-        } 
+        if(!$stmt_insert_user) 
+            $api_result["error"] = "Statement error in binding params: ".$mysqli->error;
         else 
         {
-            $mysqli = new mysqli($db_server, $db_username, $db_password, $db_name);
-
-            if($mysqli->connect_error)
-            {
-                $api_result["error"] = "DB connect error: ".$mysqli->connect_error;
+            try {
+                $result_insert_user = $stmt_insert_user->execute();
+                $api_result["success"] = true;
+                $api_result["error"] = NULL;
             }
-            else
-            {
-                $query_insert_user = "INSERT INTO tab_utenti VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $stmt_insert_user = $mysqli->prepare($query_insert_user);
-
-                if(!$stmt_insert_user)
-                    $api_result["error"] = "Statement prepare error: ".$mysqli->error;
-                else
-                {
-                    $ID_utente = NULL;
-                    
-                    if($_POST["classe"] == "NO") $_POST["classe"] = NULL;
-                    if($_POST["citta"] == "NO") $_POST["citta"] = NULL;
-
-                    $stmt_insert_user->bind_param("isssissss", 
-                        $ID_utente,
-                        $_POST["username"],
-                        password_hash($_POST["password"], PASSWORD_DEFAULT),
-                        $_POST["email"],
-                        $_POST["userlevel"],
-                        $_POST["classe"],
-                        $_POST["nome"],
-                        $_POST["cognome"],
-                        $_POST["citta"]);
-
-                    if(!$stmt_insert_user) 
-                        $api_result["error"] = "Statement error in binding params: ".$mysqli->error;
-                    else 
-                    {
-                        try {
-                            $result_insert_user = $stmt_insert_user->execute();
-                            $api_result["success"] = true;
-                            $api_result["error"] = NULL;
-                        }
-                        catch(Exception $e) {
-                            $api_result["error"] = "Statement execute error: ".$e->getMessage();
-                        }
-                        $stmt_insert_user->close();
-                    }
-                } 
+            catch(Exception $e) {
+                $api_result["error"] = "Statement execute error: ".$e->getMessage();
             }
-            $mysqli->close();
+            $stmt_insert_user->close();
         }
+    } 
+        
+    if(null !== $mysqli) {
+        $mysqli->close();
     }
 
     echo json_encode($api_result);
