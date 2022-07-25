fed1c86cf5dea090e28526fee80f4db055168f62
Alberto Tontoni <tontonialberto@outlook.it>
2022-07-25 16:32:57 +0200

    Improve if-else structure

--- backend/php/api/login_verify.api.php
+++ backend/php/api/login_verify.api.php
@@ -11,73 +11,82 @@
         "error" => "Empty Request"
     );
 
-    if(isset($_POST["username"], $_POST["password"]))
-    {
+    $error_happened = false;
+
+    if(!isset($_POST["username"], $_POST["password"])) {
+        $error_happened = true;
+    }
+
+    $mysqli = null;
+    if(!$error_happened) {
         $mysqli = new mysqli($db_server, $db_username, $db_password, $db_name);
 
-        if($mysqli->connect_error) 
-        {
+        if($mysqli->connect_error) {
             $api_result["error"] = "Database connection error: ".$mysqli->connect_error;
-        }    
-        else 
-        {
-            $query_login = "SELECT userlevel, password FROM tab_utenti WHERE username = ?";
-            $stmt_login = $mysqli->prepare($query_login);
-
-            if(!$stmt_login)
-            {
-                $api_result["error"] = "Query Error: ".$mysqli->error;
-            }
-            else 
-            {
-                $stmt_login->bind_param("s", $_POST["username"]);
-                
-                if(!$stmt_login)
-                {
-                    $api_result["error"] = "Errore nel binding dei parametri: ".$stmt_login->error;
-                }
-                else
-                {
-                    if(!$stmt_login->execute())
-                    {
-                        $api_result["error"] = "Errore nell'esecuzione dello statement: ".$stmt_login->error;
-                    }
-                    else
-                    {
-                        $result_login = $stmt_login->get_result();
-                        if(!$result_login->num_rows)
-                        {
-                            $api_result["error"] = "Errore: username errato";
-                        }
-                        else
-                        {
-                            $user_row = $result_login->fetch_array();
-                            
-                            if(!password_verify($_POST["password"], $user_row["password"]))
-                            {
-                            	$api_result["error"] = "Errore: password errata";
-                            }
-                            else
-                            {
-                            	$userlevel = $user_row["userlevel"];
-
-                                $userData = array(
-                                    "username" => $_POST["username"],
-                                    "userlevel" => $userlevel
-                                );
-
-                                global $jwt_server_key;
-                                $token = JWT::encode($userData, $jwt_server_key);
-
-                                $api_result["success"] = true;
-                                $api_result["token"] = $token;
-                                $api_result["error"] = NULL;
-                            }
-                        }
-                    }
-                }
-            }
+            $error_happened = true;
         }
+    }
+    
+    if(!$error_happened) {
+        $query_login = "SELECT userlevel, password FROM tab_utenti WHERE username = ?";
+        $stmt_login = $mysqli->prepare($query_login);
+
+        if(!$stmt_login) {
+            $api_result["error"] = "Query Error: ".$mysqli->error;
+            $error_happened = true;
+        }
+    }
+
+    if(!$error_happened) {
+        if(!$stmt_login->bind_param("s", $_POST["username"])) {
+            $api_result["error"] = "Errore nel binding dei parametri: ".$stmt_login->error;
+            $error_happened = true;
+        }
+    }
+    
+    if(!$error_happened) {
+        if(!$stmt_login->execute()) {
+            $api_result["error"] = "Errore nell'esecuzione dello statement: ".$stmt_login->error;
+            $error_happened = true;
+        }
+    }
+    
+    if(!$error_happened) {
+        $result_login = $stmt_login->get_result();
+        $wrongUsername = (0 == $result_login->num_rows);
+
+        if($wrongUsername) {
+            $api_result["error"] = "Wrong username or password";
+            $error_happened = true;
+        }
+    }
+    
+    if(!$error_happened) {
+        $user_row = $result_login->fetch_array();
+        
+        if(!password_verify($_POST["password"], $user_row["password"])) {
+            $api_result["error"] = "Wrong username or password";
+            $error_happened = true;
+        }
+    }
+    
+    if(!$error_happened) {
+        $userlevel = $user_row["userlevel"];
+
+        $userData = array(
+            "username" => $_POST["username"],
+            "userlevel" => $userlevel
+        );
+
+        global $jwt_server_key;
+        $token = JWT::encode($userData, $jwt_server_key);
+
+        $api_result["success"] = true;
+        $api_result["token"] = $token;
+        $api_result["error"] = NULL;
+    }
+
+    if(null !== $mysqli) {
         $mysqli->close();
     }
 
