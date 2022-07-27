b6864485afd08ec6c8e52b6ee2bcb9a24e8d4ade
Alberto Tontoni <tontonialberto@outlook.it>
2022-07-28 00:53:24 +0200

    Add mock id generator for Pizza objects

--- frontend/appizza/src/app/_models/pizza.model.ts
+++ frontend/appizza/src/app/_models/pizza.model.ts
@@ -1,7 +1,14 @@
 export class Pizza {
+
+    public id: number;
+
     constructor(public name: string,
                 public price: number,
                 public available: boolean,
                 public description: string,
-                public imagePath: string) {}
+                public imagePath: string) {
+                    this.id = Pizza.idCounter++;
+                }
+
+    private static idCounter = 0;
 }
